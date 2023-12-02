9e458632c0375bf3345014471150c23cc1b7877e
chado <a.sale@outlook.fr>
2023-12-02 17:19:33 +0100

    Feat: Create component for main section with exemple

---
+++ src/components/organisms/MainComponent.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import WeatherIcon from "@/components/atoms/WeatherComponent.vue";
+</script>
+
+<template>
+  <main class="flex justify-between p-40">
+    <div class="bg-[#24343D] w-fit rounded-3xl p-2">
+      <WeatherIcon />
+      <div class="mb-5 mx-12 mt-20">
+        <h2 class="text-white text-3xl">Ville de ?</h2>
+        <h3 class="text-white text-3xl">wind speed : 11km</h3>
+        <h3 class="text-white text-3xl">tuesday: 19h52</h3>
+        <h3 class="text-white text-3xl">temperature : 11°</h3>
+        <h3 class="text-white text-3xl">cloudy</h3>
+      </div>
+    </div>
+    <div class="bg-[#24343D] w-fit rounded-3xl p-2">
+      <WeatherIcon />
+      <div class="mb-5 mx-12 mt-20">
+        <h2 class="text-white text-3xl">Ville de ?</h2>
+        <h3 class="text-white text-3xl">wind speed : 11km</h3>
+        <h3 class="text-white text-3xl">tuesday: 19h52</h3>
+        <h3 class="text-white text-3xl">temperature : 11°</h3>
+        <h3 class="text-white text-3xl">cloudy</h3>
+      </div>
+    </div>
+    <div class="bg-[#24343D] w-fit rounded-3xl p-2">
+      <WeatherIcon />
+      <div class="mb-5 mx-12 mt-20">
+        <h2 class="text-white text-3xl">Ville de ?</h2>
+        <h3 class="text-white text-3xl">wind speed : 11km</h3>
+        <h3 class="text-white text-3xl">tuesday: 19h52</h3>
+        <h3 class="text-white text-3xl">temperature : 11°</h3>
+        <h3 class="text-white text-3xl">cloudy</h3>
+      </div>
+    </div>
+    <div class="bg-[#24343D] w-fit rounded-3xl p-2">
+      <WeatherIcon />
+      <div class="mb-5 mx-12 mt-20">
+        <h2 class="text-white text-3xl">Ville de ?</h2>
+        <h3 class="text-white text-3xl">wind speed : 11km</h3>
+        <h3 class="text-white text-3xl">tuesday: 19h52</h3>
+        <h3 class="text-white text-3xl">temperature : 11°</h3>
+        <h3 class="text-white text-3xl">cloudy</h3>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
