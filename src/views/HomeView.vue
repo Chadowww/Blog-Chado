62a2f107bef8dba2f89bef2aef6626d6d3a9904d
chado <a.sale@outlook.fr>
2023-12-02 17:15:35 +0100

    Feat: Create home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
-
 <script lang="ts">
+import Navbar from "@/components/organisms/Navbar.vue";
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TitleComponent from "@/components/atoms/TitleComponent.vue";
+import FilterComponent from "@/components/molecules/FilterComponent.vue";
+import MainComponent from "@/components/organisms/MainComponent.vue";
+
+let apiKey = process.env.VUE_APP_API_KEY;
+console.log(apiKey);
 
 @Options({
   components: {
-    HelloWorld,
+    FilterComponent,
+    TitleComponent,
+    MainComponent,
+    Navbar,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+
+<template>
+  <Navbar />
+  <div class="home">
+    <TitleComponent />
+    <FilterComponent />
+    <MainComponent />
+  </div>
+</template>
+
+<style>
+body {
+  background-image: url("../assets/background.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: #1b262c;
+  min-height: 100vh;
+}
+</style>
