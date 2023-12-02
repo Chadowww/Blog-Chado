9bb42b5f06ece35e06b9384a0999ab5b7f1b0d8a
chado <a.sale@outlook.fr>
2023-12-02 17:17:09 +0100

    Feat: create title component

---
+++ src/components/atoms/TitleComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "TitleComponent",
+});
+</script>
+
+<template>
+  <h1
+    class="w-8/12 my-16 mx-auto text-white text-center text-6xl font-extrabold"
+  >
+    Seeing the weather of the whole world with <span>Dark Weather!</span>
+  </h1>
+</template>
+
+<style scoped>
+span {
+  background: linear-gradient(91deg, #caecff 39.76%, #78cfff 86.12%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-style: normal;
+  line-height: 65px;
+}
+</style>
