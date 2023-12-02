63a427e80c35149a064680a90572e4d56a9e1698
chado <a.sale@outlook.fr>
2023-12-02 17:18:06 +0100

    Feat: Create component for weather icon

---
+++ src/components/atoms/WeatherComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative h-48">
+    <div
+      class="weather-icon absolute right-0 top-[-50%] rounded-full bg-yellow-500 w-64 h-64"
+    >
+      <div
+        class="absolute bottom-6 left-[-40px] rounded-l-full rounded-t-full bg-white w-40 h-40"
+      ></div>
+      <div
+        class="absolute bottom-6 left-16 rounded-r-full bg-white w-28 h-28"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
