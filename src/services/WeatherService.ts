4fed14006645dd08cd61751e7bb0a1cde6033a9b
chado <a.sale@outlook.fr>
2023-12-03 13:14:46 +0100

    Feat: Create new service for manage call to API

---
+++ src/services/WeatherService.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+class WeatherService {
+  async getWeatherByCity(city: string) {
+    const params = {
+      appid: process.env.VUE_APP_API_KEY,
+      q: city,
+      units: "metric",
+    };
+
+    try {
+      const response = await axios.get(
+        "https://api.openweathermap.org/data/2.5/weather",
+        {
+          params,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default new WeatherService();
