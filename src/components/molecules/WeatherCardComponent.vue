703fad00d5236048b7898c0afe5015995c0da0a2
chado <a.sale@outlook.fr>
2023-12-03 12:29:07 +0100

    Feat: Created New component with props

---
+++ src/components/molecules/WeatherCardComponent.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import WeatherComponent from "@/components/atoms/WeatherComponent.vue";
+
+export default {
+  components: {
+    WeatherComponent,
+  },
+  props: {
+    weather: [Object, Array],
+  },
+};
+</script>
+<template>
+  <div class="bg-[#24343D] w-fit rounded-3xl p-2 md:scale-75">
+    <WeatherComponent />
+    <div class="mb-5 mx-12 mt-20">
+      <h2 class="text-white text-3xl">{{ weather?.name }}</h2>
+      <h3 class="text-white text-3xl">
+        temperature actuelle: {{ weather?.main?.temp }}°
+      </h3>
+      <h3 class="text-white text-3xl">
+        Min : {{ weather?.main?.temp_min }}° / Max :
+        {{ weather?.main?.temp_max }}°
+      </h3>
+      <h3 class="text-white text-3xl">
+        wind speed : {{ weather?.wind?.speed }} km/h
+      </h3>
+
+      <h3 class="text-white text-3xl">{{ weather.weather[0]?.description }}</h3>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
