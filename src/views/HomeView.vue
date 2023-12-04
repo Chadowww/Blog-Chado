91ee700e3fdcba43dc8c895e4cc5b07b183fe4eb
chado <a.sale@outlook.fr>
2023-12-04 09:15:18 +0100

    Feat: Add propos for MainComponent & Add onFilterChanged to update what city will see

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
-import Navbar from "@/components/organisms/Navbar.vue";
 import { Options, Vue } from "vue-class-component";
+import Navbar from "@/components/organisms/Navbar.vue";
 import TitleComponent from "@/components/atoms/TitleComponent.vue";
 import FilterComponent from "@/components/molecules/FilterComponent.vue";
 import MainComponent from "@/components/organisms/MainComponent.vue";
+import WeatherService from "@/services/WeatherService";
 
 @Options({
   components: {
@@ -13,15 +14,37 @@ import MainComponent from "@/components/organisms/MainComponent.vue";
     Navbar,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  private weathers: any[] = [];
+
+  async mounted() {
+    const locations = ["New York", "Paris", "Tokyo", "Jakarta"];
+
+    if (this.weathers.length === 0) {
+      for (const location of locations) {
+        const weather = await WeatherService.getWeatherByCity(location);
+        this.weathers.push(weather);
+      }
+      return;
+    }
+
+    const weather = await WeatherService.getWeatherByCity(this.weathers[0]);
+    this.weathers.push(weather);
+  }
+  async onFilterChanged(filter: string) {
+    console.log(filter);
+    this.weathers = [await WeatherService.getWeatherByCity(filter)];
+    console.log(this.weathers);
+  }
+}
 </script>
 
 <template>
   <Navbar />
   <div class="home">
     <TitleComponent />
-    <FilterComponent />
-    <MainComponent />
+    <FilterComponent @filter-changed="onFilterChanged" />
+    <MainComponent :weathers="weathers" />
   </div>
 </template>
 
