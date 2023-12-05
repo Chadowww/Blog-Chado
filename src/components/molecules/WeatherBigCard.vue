0eb15d5c117615a9365ae687d880b1049926dd64
chado <a.sale@outlook.fr>
2023-12-05 14:49:16 +0100

    Feat: Add new card for search result filter

---
+++ src/components/molecules/WeatherBigCard.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import WeatherCardComponent from "@/components/molecules/WeatherCardComponent.vue";
+import LineChart from "@/components/atoms/LineChart.vue";
+import WeatherService from "@/services/WeatherService";
+@Options({
+  name: "WeatherBigCard",
+  components: { WeatherCardComponent, LineChart },
+  props: {
+    weathers: {
+      type: Array,
+      required: true,
+    },
+  },
+})
+export default class WeatherBigCard extends Vue {
+  private weatherForcast: { list: Array<any> } | null = null;
+  async mounted() {
+    const wheather = await WeatherService.getWeatherForecastByCity(
+      this.weathers[0].name
+    );
+    this.weatherForcast = wheather;
+  }
+}
+</script>
+
+<template>
+  <div
+    class="flex flex-row justify-around items-center bg-[#24343D] rounded-3xl p-2"
+  >
+    <WeatherCardComponent
+      v-for="weather in weathers"
+      :key="weather.name"
+      :weather="weather"
+    />
+    <LineChart v-if="weatherForcast" :previous="weatherForcast.list" />
+  </div>
+</template>
+
+<style scoped></style>
