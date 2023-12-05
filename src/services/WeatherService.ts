addc40ed05759b8853b36fa9c92b991993b7698d
chado <a.sale@outlook.fr>
2023-12-05 14:50:55 +0100

    Feat: Add new function to get  previous data

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -20,6 +20,26 @@ class WeatherService {
       console.error(error);
     }
   }
+
+  async getWeatherForecastByCity(city: string) {
+    const params = {
+      appid: process.env.VUE_APP_API_KEY,
+      q: city,
+      units: "metric",
+    };
+
+    try {
+      const response = await axios.get(
+        "https://api.openweathermap.org/data/2.5/forecast",
+        {
+          params,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default new WeatherService();
