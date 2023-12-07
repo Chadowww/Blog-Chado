1ad354bfb304d429608a7c62b216d821df1b5354
chado <a.sale@outlook.fr>
2023-12-07 12:31:31 +0100

    Feat: Create new page for contact

---
+++ src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import NavBarComponent from "@/components/organisms/NavbarComponent.vue";
+import FooterComponent from "@/components/organisms/FooterComponent.vue";
+import FormContactComponent from "@/components/organisms/FormContactComponent.vue";
+</script>
+
+<template>
+  <section class="h-screen flex flex-col justify-between">
+    <NavBarComponent />
+    <FormContactComponent />
+    <FooterComponent />
+  </section>
+</template>
+
+<style scoped></style>
