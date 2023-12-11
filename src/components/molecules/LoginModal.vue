d5e5fe349532af0aa3b5da298c021eb3c481f4cb
chado <a.sale@outlook.fr>
2023-12-11 13:16:47 +0100

    Feat: Add login asynchron function and add new component for input email/password

--- src/components/molecules/LoginModal.vue
+++ src/components/molecules/LoginModal.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { defineProps, defineEmits } from "vue";
+import InputPassword from "@/components/atoms/InputPassword.vue";
+import InputEmail from "@/components/atoms/InputEmail.vue";
+import axios from "axios";
 
 const props = defineProps({
   toggleLogin: Boolean,
@@ -7,6 +10,36 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["update:toggleLogin", "update:toggleRegister"]);
+
+type dataType = {
+  email: string;
+  password: string | null;
+};
+
+const data: dataType = {
+  email: "",
+  password: "",
+};
+
+const login = async () => {
+  console.log(data.email, data.password);
+  try {
+    const response = await axios.post(
+      "https://127.0.0.1:8000/login",
+      {
+        email: data.email,
+        password: data.password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <template>
@@ -19,38 +52,12 @@ const emit = defineEmits(["update:toggleLogin", "update:toggleRegister"]);
       >
         Sign in to your account
       </h1>
-      <form class="space-y-4 md:space-y-6" action="#">
+      <form class="space-y-4 md:space-y-6" @submit.prevent="login">
         <div>
-          <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@company.com"
-            required=""
-          />
+          <input-email v-model:email="data.email" />
         </div>
         <div>
-          <label
-            for="password"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
-          />
+          <input-password v-model:password="data.password" />
         </div>
         <div class="flex items-center justify-between">
           <div class="flex items-start">
