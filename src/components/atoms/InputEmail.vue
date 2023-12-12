9e47bff9d3e1b13ae703f456ef1a9efca0e65e04
chado <a.sale@outlook.fr>
2023-12-12 14:07:23 +0100

    Feat: Add new propos to get error from the parent

--- src/components/atoms/InputEmail.vue
+++ src/components/atoms/InputEmail.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { errors, verifEmail } from "@/utils/formValidations";
-import { defineProps, defineEmits, reactive } from "vue";
+import { defineProps, defineEmits, reactive, ref } from "vue";
 
 const props = defineProps({
   email: String,
+  emailError: String,
 });
 
 const data = reactive({
@@ -31,17 +32,17 @@ const emit = defineEmits(["update:email"]);
     id="email"
     class="shadow-sm bg-gray-50 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light focus:outline-none"
     :class="{
-      'border-2 border-red-500': errors.email,
+      'border-2 border-red-500': errors.email || emailError,
       'border-2 border-green-300': errors.email === null,
     }"
     placeholder="name@company.com"
     required=""
   />
   <div
-    v-if="errors.email"
+    v-if="errors.email || emailError"
     class="border-red-600 border-2 rounded-lg p-2 mt-2 text-red-600 dark:text-red-400 dark:border-red-400"
   >
-    {{ errors.email }}
+    {{ errors.email || emailError }}
   </div>
 </template>
 
